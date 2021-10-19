c9f3f65ba513c1fd46271b5d7df31584f42db5fc
Anil Bhasi <anilbhasib@gmail.com>
2021-10-19 14:31:56 +0530

    api connected

--- src/Tab/Category.js
+++ src/Tab/Category.js
@@ -1,12 +1,37 @@
 import React, { Component } from 'react'
-import {View,Text,StyleSheet} from 'react-native'
+import {View,Text,StyleSheet,ActivityIndicator} from 'react-native'
 
 
 
 class Categoty extends Component {
+
+    constructor(){
+        super()
+        this.state={
+            loader:false
+        }
+
+    }
+
+    componentDidMount(){
+        this.getData()
+    }
+
+    getData(){
+        fetch('https://api.sampleapis.com/coffee/hot')
+        .then((response)=>response.json())
+        .then((response)=>{
+            console.log('Your response is: ',response);
+        })
+        .catch((error)=>{
+            console.log('Eroor is :',error);
+        })
+    }
+
     render() {
         return (
             <View style={styles.container}>
+                <ActivityIndicator size={50} color='grey' animating={true}/>
                 <Text style={styles.homeText}>Hi this is Categoty page</Text>
 
             </View>
